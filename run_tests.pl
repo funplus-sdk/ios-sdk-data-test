2ac6ca616ce8483c018a3f00705b887b472e3be2
yuankun.zhang <fmcocoa@gmail.com>
2016-11-16 21:16:23 +0800

    run_tests.pl

--- run_tests.pl
+++ run_tests.pl
@@ -6,11 +6,6 @@ use Cwd qw( abs_path );
 use File::Basename qw( dirname );
 use File::Tee qw( tee );
 
-die('Please set the $simulator_id first!') if (! defined $ENV{'simulator_id'});
-
-my $simulator_id = $ENV{'simulator_id'};
-print("SIMULATOR ID: $simulator_id\n");
-
 # Change working directory.
 my $cwd = dirname(abs_path($0));
 chdir $cwd;
@@ -22,5 +17,5 @@ my $dt = strftime("%Y%m%d", localtime);
 my $logfile = "log/$dt-success.log";
 tee STDOUT, '>>', $logfile;
 
-system("xcodebuild test -project ../../FunPlusSDK/FunPlusSDK.xcodeproj/ -scheme DataTestApp -destination 'platform=iOS Simulator,id=$simulator_id'");
+system("xcodebuild test -project DataTestApp.xcodeproj/ -scheme DataTestApp -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.1'");
 exit $? >> 8;
